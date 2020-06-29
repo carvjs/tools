bb9b67d16fa8eaa60817d87ec292dbfc7f24e266
Sascha Tandel <s.tandel@kenoxa.de>
2020-06-29 23:14:25 +0200

    feat(eslint): add typescript

--- packages/eslint-config/eslint-preset.js
+++ packages/eslint-config/eslint-preset.js
@@ -59,6 +59,19 @@ module.exports = {
     //   },
     // },
     {
+      files: ['**/*.ts', '**/*.tsx'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: ['./tsconfig.json'],
+      },
+      plugins: ['@typescript-eslint'],
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'prettier/@typescript-eslint',
+      ],
+    },
+    {
       files: [
         '**/*.test.js',
         '**/*.spec.jsx',
